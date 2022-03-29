a19b102519036f3d9c8e316744a204495b816ecb
RafaelaSSantos <rafaela.santos@nava.com.br>
2022-03-29 15:24:03 -0300

    primeiro

---
+++ angular/primeiro-exemplo/meu-app/src/app/segundo/segundo.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-segundo',
+  template: `
+  <div>{{preco}}</div>
+  <div>{{preco + 1}}</div>
+  <div>{{preco * 20}}</div>
+  <div>{{preco / 10}}</div>
+  <div>{{user.nome}}</div>
+  <div>{{getPreco()}}</div>
+  <div>{{preco > 0}}</div>
+  `,
+})
+export class SegundoComponent{
+  public user: {id:number; nome:string} = {
+    id: 3045,
+    nome:' Rafa'
+  };
+  public preco: number = 10;
+
+  getPreco(): string {
+    return `R$ ${this.preco}`;
+  }
+}
