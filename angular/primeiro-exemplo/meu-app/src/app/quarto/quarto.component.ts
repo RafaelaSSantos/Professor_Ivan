9df7a25fa71f5f40b69ee2623aa25d779fd8a005
RafaelaSSantos <rafaela.santos@nava.com.br>
2022-03-30 16:43:50 -0300

    evento blur em uma div

--- angular/primeiro-exemplo/meu-app/src/app/quarto/quarto.component.ts
+++ angular/primeiro-exemplo/meu-app/src/app/quarto/quarto.component.ts
@@ -3,17 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-quarto',
   template: `
-    <div>
-      <input type="text" placeholder="Senha" (blur) = 'onBlur()' #campoInput >
-      <button type="button" (click)="onClick(campoInput)">CLIQUE</button>
-    </div>
-  `,
+  <div>
+    <input type="text" placeholder="Senha" (blur) = 'onBlur()' #campoInput >
+    <button type="button"  placeholder="confirma Senha" (click)="onClick(campoInput)">CLIQUE</button>
+  </div>
+`,
 })
 export class QuartoComponent {
-  public onBlur(): void {
-    console.log('O foco está perdido para a senha');
+public onBlur(): void {
+  console.log('O foco está perdido para a senha');
 }
-  public onClick(campoInput: HTMLInputElement): void {
-    console.log(campoInput.value);
-  }
+public onClick(campoInput: HTMLInputElement): void {
+  console.log(campoInput.value);
+}
 }
