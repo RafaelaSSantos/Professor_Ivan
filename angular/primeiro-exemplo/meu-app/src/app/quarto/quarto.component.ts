9b4d7caf66378a4aa0b0ce2b44ae3a940202590c
RafaelaSSantos <rafaela.santos@nava.com.br>
2022-03-30 16:49:39 -0300

    evento blur em uma para verificar senha

--- angular/primeiro-exemplo/meu-app/src/app/quarto/quarto.component.ts
+++ angular/primeiro-exemplo/meu-app/src/app/quarto/quarto.component.ts
@@ -4,15 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-quarto',
   template: `
   <input type="text" placeholder="Senha" #campoSenha />
-  <input type="text" placeholder="Confirmar senha"
-    #campoVerificaSenha (blur)="verificadorDeSenha(campoSenha.value, campoVerificaSenha.value)"
-  />
+  <input type="text" placeholder="Confirmar senha" #campoVerificaSenha (blur)="verificadorSenha(campoSenha.value, campoVerificaSenha.value)"/>
   <div>{{ textoSenha }}</div>
 `,
 })
 export class QuartoComponent {
 public textoSenha: string = '';
-public verificadorDeSenha(senha: string, verificaSenha: string): void {
+public verificadorSenha(senha: string, verificaSenha: string): void {
   if (senha !== verificaSenha)
     this.textoSenha = 'Senhas são diferentes';
   else this.textoSenha = '';
